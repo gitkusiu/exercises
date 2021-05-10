13b70a523ea51a319c23cddea32de10d15a71a2a
kusiu <kkusiu@gmail.com>
2021-05-10 10:12:19 +0200

    move and make compilable thread excecises

--- c++/c++11/thread/vector_of_threads/vector_of_threads.cpp
+++ c++/c++11/thread/vector_of_threads/vector_of_threads.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <thread>
 #include <algorithm>
+#include <vector>
+#include <functional>
+
 class WorkerThread
 {
 public:
-    void operator()()     
+    bool operator()()
     {
         std::cout<<"Worker Thread "<<std::this_thread::get_id()<<" is Executing"<<std::endl;
+        return true;
     }
 };
 int main()  
@@ -14,7 +18,7 @@ int main()
     std::vector<std::thread> threadList;
     for(int i = 0; i < 10; i++)
     {
-        threadList.push_back( std::thread( WorkerThread() ) );
+        threadList.push_back( std::thread( WorkerThread{} ) );
     }
     // Now wait for all the worker thread to finish i.e.
     // Call join() function on each of the std::thread object
